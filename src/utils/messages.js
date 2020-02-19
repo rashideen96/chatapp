e49e13d7e967a1c97f20cb286e7db92d4de41a7f
vertexPlayer <xyberworm@yahoo.com>
2020-02-20 01:38:12 +0800

    initial commit

---
+++ src/utils/messages.js
@@ -0,0 +1,16 @@
+module.exports = {
+	generateMessage: (username, text) => {
+		return {
+			username,
+			text,
+			createdAt: new Date().getTime()
+		};
+	},
+	generateLocationMessage: (username, url) => {
+		return {
+			username,
+			url,
+			createdAt: new Date().getTime()
+		};
+	}
+};
